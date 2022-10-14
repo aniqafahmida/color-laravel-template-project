849c469217e1efacecc16cd4f35e38f014d41d75
anika <anikafahmidahossain29@gmail.com>
2022-10-14 17:17:33 +0600

    color laravel commit 2nd

---
+++ public/app.js
@@ -0,0 +1,25 @@
+//on load handler create
+window.onload = () => {
+    main();
+}
+
+function main() {
+ const root =document.getElementById('root');
+ const btn = document.getElementById('change-btn');
+
+ btn.addEventListener('click' ,function(){
+     const bgColor = generateRGBColor()
+     root.style.backgroundColor = bgColor ;
+ })
+}
+
+// random color gggeneration 
+//rgb(0-255,0-255,0-255)
+
+function generateRGBColor () {
+     const red = Math.floor(Math.random() * 255);
+     const green = Math.floor(Math.random() * 255);
+     const blue = Math.floor(Math.random() * 255);
+
+     return `rgb(${red} , ${green} , ${blue})`
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('style.css')}}">
+    <script src="{{asset('/app.js')}}" defer></script>
+    <title>change background color</title>
+
+</head>
+<body>
+    
+    <div class="container" id="root">
+        <h1> change background color by cicking  Button</h1>
+    </div>
+
+    <button id="change-btn">Change Color</button>
+</body>
+</html>
